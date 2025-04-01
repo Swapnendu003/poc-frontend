e45f61179131891addcd535ec263192185fdf59a
Swapnendu003 <swaps.b003@gmail.com>
2025-04-02 01:49:33 +0530

    Hosted the backend in azure and changed the base URL

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://poc-backend-ctxm.onrender.com";
-export const WS_URL = "wss://poc-backend-ctxm.onrender.com";
+export const API_BASE_URL = "https://poc-backend2.azurewebsites.net";
+export const WS_URL = "wss://poc-backend2.azurewebsites.net";
 export const RECONNECT_DELAY = 5000;
